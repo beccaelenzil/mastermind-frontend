c3a782d0d87be4d097ad3d4092a45958f3e8ebbb
Becca Elenzil <beccaelenzil@gmail.com>
2023-01-14 16:31:33 -0800

    fix typo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Mastermind
 
-To play the game, visit the app deployed to github pages: [https://beccaelenzil.github.io/mastermind-frontend/](https://beccaelenzil.github.io/mastermind-frontend/)
+To play the game, visit the app deployed to GitHub pages: [https://beccaelenzil.github.io/mastermind-frontend/](https://beccaelenzil.github.io/mastermind-frontend/)
 
 For more information about this project, go to [https://github.com/beccaelenzil/mastermind-backend](https://github.com/beccaelenzil/mastermind-backend)
 
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -22,12 +22,12 @@ const Instructions = () => {
         <section id="rules" className="game-body">
           <h1 id="rules-title">Game Rules</h1>
           <p id="rules-text">
-            At the start of the game the computer will randomly select a pattern
-            of symbols. The length of the pattern and the number of distinct
-            symbols depends on the level you select (easy, standard, or hard).
-            You will have 10 attempts to guess the pattern. At the end of each
-            guess, the computer wisll display two numbers. The first number is
-            the number of symbols guessed correctly (not necessarily in the
+            At the start of the game, the computer will randomly select a
+            pattern of symbols. The length of the pattern and the number of
+            distinct symbols depends on the level you select (easy, standard, or
+            hard). You will have 10 attempts to guess the pattern. At the end of
+            each guess, the computer will display two numbers. The first number
+            is the number of symbols guessed correctly (not necessarily in the
             correct position). The second number is the number of symbols that
             are guessed correctly and in the correct position. Below are a few
             example games.
